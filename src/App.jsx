5d3311d5d5eb663f0406e90999414346e4a73afc
Aegnis <pierrick.leclercq.dev@gmail.com>
2023-03-06 16:57:13 +0100

    fin de journée

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import style from "./App.module.css"
 export function App (){ 
     return (
         <div className={style.main_container}> 
-            <div className={style.header}></div> 
-            <div className={style.tv_show_detail}></div> 
-            <div className={style.recommentations}></div> 
+            <div className={style.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-md-12 col-lg-4">
+                        <input type="text" />
+                    </div>
+                </div>
+            </div> 
+            <div className={style.tv_show_detail}>Details</div> 
+            <div className={style.recommentations}>Recommendation</div> 
         </div> 
     );
 } 
