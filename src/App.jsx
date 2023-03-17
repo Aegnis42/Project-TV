9b75c0e70543958e0d8e6acacdc91bda5669d859
Aegnis42 <pierrick.leclercq.pro@gmail.com>
2023-03-17 09:38:16 +0100

    start days

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./global.css"
 import style from "./App.module.css"
 import { TVShowAPI } from "./api/tv-show";
 import { useEffect, useState } from "react";
+import { BACKDROP_BASE_URL } from "./config";
 
 
 export function App (){ 
@@ -19,7 +20,10 @@ export function App (){
     }, []);
     console.log("***", currentTVShow)
     return (
-        <div className={style.main_container}> 
+        <div className={style.main_container}
+        style = {{background:currentTVShow
+            ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+            : "black",}}> 
             <div className={style.header}>
                 <div className="row">
                     <div className="col-4">
